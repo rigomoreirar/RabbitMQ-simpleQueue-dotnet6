a6b952aaf3d38011496c2ee6435f3e9912b648f1
rigomoreirar <rigomoreirar@gmail.com>
2024-04-09 05:48:33 -0600

    Added new project folder

---
+++ SimpleProducerToConsumer/RabbitMQ.Consumer/RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMQ.Consumer
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory { Uri = new Uri("amqp://guest@localhost:5672") };
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare("demo-queue",
+                                         durable: true,
+                                         exclusive: false,
+                                         autoDelete: false,
+                                         arguments: null);
+
+                    var consumer = new EventingBasicConsumer(channel);
+
+                    consumer.Received += (sender, ea) =>
+                    {
+                        var body = ea.Body.ToArray();
+                        var messageJson = Encoding.UTF8.GetString(body);
+                        var message = JsonConvert.DeserializeObject<dynamic>(messageJson);
+
+                        Console.WriteLine($"Received message: {message.Name} - {message.Message}");
+                    };
+
+                    channel.BasicConsume("demo-queue", true, consumer);
+                    Console.ReadLine();
+                }
+            }
+            
+        }
+    }
+}
---
+++ SimpleProducerToConsumer/RabbitMQ.Producer/RabbitMQ.Producer/Program.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMQ.Producer
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory { Uri = new Uri("amqp://guest@localhost:5672") };
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare("demo-queue", 
+                                         durable: true, 
+                                         exclusive: false, 
+                                         autoDelete: false, 
+                                         arguments: null);
+
+                    var message = new { Name = "Producer", Message = "Hello!" };
+
+                    var messageJson = JsonConvert.SerializeObject(message);
+
+                    var body = Encoding.UTF8.GetBytes(messageJson);
+
+                    channel.BasicPublish("", "demo-queue", null, body);
+
+                    Console.WriteLine("Message Sent");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
